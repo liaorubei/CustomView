bedc1c730362d28393b26bacee01479715cda3dd
liaorubei@outlook.com <liaorubei@outlook.com>
2015-07-14 16:33:02 +0800

    -

--- 开关按钮/src/main/java/com/newclass/switchbutton/MainActivity.java
+++ 开关按钮/src/main/java/com/newclass/switchbutton/MainActivity.java
@@ -1,27 +1,36 @@
 package com.newclass.switchbutton;
 
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
-
-import java.util.List;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //写个中文看看怎么滴
 
-        List<PackageInfo> packages = getPackageManager().getInstalledPackages(PackageManager.GET_SIGNATURES);
-        for (PackageInfo aPackage : packages) {
-            System.out.println("packageName=" + aPackage.packageName + " versionName=" + aPackage.versionName);
-            String dataDir = aPackage.applicationInfo.dataDir;
-        }
+        //延时发出消息
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, "something", Toast.LENGTH_SHORT).show();
+            }
+        }, 1000);
+
+        //退出本Activity
+        finish();
+
     }
-
-
 }
