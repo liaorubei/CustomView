dcec84bf9526d69ad7a91e1f8efdc74d857ea13b
liaorubei@outlook.com <liaorubei@outlook.com>
2015-07-14 14:34:52 +0800

    改了编码

--- 开关按钮/src/main/java/com/newclass/switchbutton/MainActivity.java
+++ 开关按钮/src/main/java/com/newclass/switchbutton/MainActivity.java
@@ -1,7 +1,11 @@
 package com.newclass.switchbutton;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,6 +15,12 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //写个中文看看怎么滴
+
+        List<PackageInfo> packages = getPackageManager().getInstalledPackages(PackageManager.GET_SIGNATURES);
+        for (PackageInfo aPackage : packages) {
+            System.out.println("packageName=" + aPackage.packageName + " versionName=" + aPackage.versionName);
+            String dataDir = aPackage.applicationInfo.dataDir;
+        }
     }
 
 
